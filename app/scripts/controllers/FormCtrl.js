cb8a12b3f5335ee3c612b6583b8aa2d1de51c10d
Joona Eronen <ojoer003@edu.xamk.fi>
2017-11-20 00:25:13 +0200

    Hups konflitki

--- app/scripts/controllers/FormCtrl.js
+++ app/scripts/controllers/FormCtrl.js
@@ -20,26 +20,13 @@ angular.module('xamkTilanvarausApp')
       url: "http://localhost:8000/haeVarausTiedotLomakkeelle",
       data: { id: localStorage.getItem("id") }
     })
-<<<<<<< HEAD
       .then(function (response) { // Onnistunut http-kutsu (success)
 
         $scope.yhteenveto = response.data;
 
-
-        console.log($scope.yhteenveto)
-
       })
       .catch(function (response) { // error
 
-=======
-    .then(function (response) { // Onnistunut http-kutsu (success)
-      
-      $scope.yhteenveto = response.data;
-                
-    })
-    .catch(function (response) { // error
-      
->>>>>>> 622aeba6a6cc30583a3d8b16981ed3e1a711c955
         $scope.virheteksti = response.data.virhe;
 
         $("#virheIkkuna").modal("show");
@@ -56,21 +43,15 @@ angular.module('xamkTilanvarausApp')
       console.log($scope.yhteenveto)
 
       $http({
-<<<<<<< HEAD
         method: "POST",
         url: "http://localhost:8000/tallennaVaraus",
-        data: $scope.varaus
-=======
-        method : "POST",
-        url : "http://localhost:8000/tallennaVaraus",
         data: data
       })
-      .then(function (response) { // Onnistunut http-kutsu (success)
-          
-        console.log(response.data);
-                  
->>>>>>> 622aeba6a6cc30583a3d8b16981ed3e1a711c955
-      })
+        .then(function (response) { // Onnistunut http-kutsu (success)
+
+          console.log(response.data);
+
+        })
         .then(function (response) { // Onnistunut http-kutsu (success)
 
           console.log(response.data);
@@ -82,29 +63,6 @@ angular.module('xamkTilanvarausApp')
 
           $("#virheIkkuna").modal("show");
 
-<<<<<<< HEAD
         });
-
-      $http({
-        method: "POST",
-        url: "http://localhost:8000/tallennaKalenteriin",
-        data: $scope.yhteenveto
-      })
-        .then(function (response) { // Onnistunut http-kutsu (success)
-
-          console.log(response.data);
-
-        })
-        .catch(function (response) { // error
-
-          // $scope.virheteksti = response.data.virhe;
-          // console.log(response.data)
-
-          // $("#virheIkkuna").modal("show");
-
-        });
-=======
-      });
->>>>>>> 622aeba6a6cc30583a3d8b16981ed3e1a711c955
     };
   });
